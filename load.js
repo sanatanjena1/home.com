3804d78aecfa348b6c8ef9df070aadd2c36df88c
sanatanjena1 <sanatan.jena112@gmail.com>
2024-02-22 14:37:16 +0530

    four html & js file uplo

---
+++ load.js
@@ -0,0 +1,7 @@
+// Simulate a delay to mimic processing
+setTimeout(function() {
+    // Hide the loading container and show the main content
+    $('.loading-container').hide();
+    $('.main-content').show();
+  }, 1000); // 5000 milliseconds (5 seconds) delay
+  
